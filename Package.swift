7a0763d8af5b536e7e682e3155004a401bc66c01
iker <iker.garay@ironchip.net>
2022-07-13 20:19:58 +0200

    wip

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPackageWithFramework",
+    platforms: [
+         .macOS(.v12), .iOS(.v15)
+       ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,13 +19,10 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SwiftPackageWithFramework",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftPackageWithFrameworkTests",
-            dependencies: ["SwiftPackageWithFramework"]),
-    ]
+         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+         .binaryTarget(
+             name: "SwiftPackageWithFramework",
+             path: "./Sources/SwiftPackageWithFramework.xcframework")
+     ]
 )
---
+++ Sources/SwiftPackageWithFramework.xcframework/ios-arm64_x86_64-simulator/SwiftPackageWithFramework.framework/Headers/SwiftPackageWithFramework.h
@@ -0,0 +1,18 @@
+//
+//  SwiftPackageWithFramework.h
+//  SwiftPackageWithFramework
+//
+//  Created by Jose F Gómez Arbaizar on 13/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftPackageWithFramework.
+FOUNDATION_EXPORT double SwiftPackageWithFrameworkVersionNumber;
+
+//! Project version string for SwiftPackageWithFramework.
+FOUNDATION_EXPORT const unsigned char SwiftPackageWithFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPackageWithFramework/PublicHeader.h>
+
+
